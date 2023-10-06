5274c53991ba8536fe93d73c3202c382dc6c660e
Frants Kavalionak <frants.kavalionak@gmail.com>
2023-10-06 18:36:24 -0400

    netlify deploy ready

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+import React from "react";
+
+const random = Math.random();
+
+
+function MyQuote() {
+  if (random < 0.2) {
+    return (
+      <blockquote>
+        <p>The stars don't look bigger, but they do look brighter.</p>
+        <cite>
+          <a target="_blank" href="https://en.wikipedia.org/wiki/Sally_Ride">
+            Sally Ride
+          </a>
+        </cite>
+      </blockquote>
+    );
+  } else if (random >= 0.2 && random < 0.4){
+    return (
+        <blockquote>
+        <p>That's one small step for a man, a giant leap for mankind.</p>
+        <cite>
+          <a target="_blank" href="https://en.wikipedia.org/wiki/Neil_Armstrong">
+            Neil Armstrong
+          </a>
+        </cite>
+      </blockquote>
+    )
+  } else if (random >= 0.4 && random <0.6){
+    return (
+        <blockquote>
+        <p>The Universe is under no obligation to make sense to you.</p>
+        <cite>
+          <a target="_blank" href="https://en.wikipedia.org/wiki/Neil_deGrasse_Tyson">
+            Neil deGrasse Tyson
+          </a>
+        </cite>
+      </blockquote>
+    )
+  } else if (random >= 0.6 && random <0.8){
+    return (
+        <blockquote>
+        <p>Poyekhali! ( Let’s go!)</p>
+        <cite>
+          <a target="_blank" href="https://en.wikipedia.org/wiki/Yuri_Gagarin">
+          Yuri Gagarin
+          </a>
+        </cite>
+      </blockquote>
+    )
+  } else {
+    return (
+      <blockquote>
+      <p>The Earth is the cradle of humanity, but mankind cannot stay in the cradle forever.</p>
+      <cite>
+        <a target="_blank" href="https://en.wikipedia.org/wiki/Konstantin_Tsiolkovsky">
+        Konstantin Tsiolkovsky
         </a>
-      </header>
-    </div>
-  );
+      </cite>
+    </blockquote>
+  )
+  }
 }
 
-export default App;
+export default MyQuote;
+
+
+
---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+import React from "react";
+
+
+function Header() {
+  return <h1>Please refresh the page</h1>
+}
+
+
+export default Header;
+
+
+
